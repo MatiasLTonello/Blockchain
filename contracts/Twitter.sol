d1a94a4c21cbef24bade2cc0df86f8558412c431
MatiasLTonello <matiaslucianotonello11@gmail.com>
2024-03-24 03:26:15 -0300

    twitter like

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -11,6 +11,7 @@ contract Twitter {
         string content;
         uint256 timestamp;
         uint256 likes;
+        uint256 id;
     }
 
     address public owner;
@@ -35,6 +36,7 @@ contract Twitter {
         require(bytes(_tweet).length <= MAX_TWEET_LENGTH, "Tweet is too long!" );
 
         Tweet memory newTweet = Tweet({
+            id: tweets[msg.sender].length,
             author: msg.sender,
             content: _tweet,
             timestamp: block.timestamp,
@@ -43,6 +45,17 @@ contract Twitter {
 
         tweets[msg.sender].push(newTweet);
     }
+
+    function likeTweet(address author, uint256 id) external {
+        require(tweets[author][id].id == id, "Tweet Doesnt exists!");
+        tweets[author][id].likes++;
+    } 
+
+      function unlikeTweet(address author, uint256 id) external {
+        require(tweets[author][id].id == id, "Tweet Doesnt exists!");
+        require(tweets[author][id].likes > 0, "Tweet doesnt have likes");
+        tweets[author][id].likes--;
+    } 
 
     function getTweet(uint _i) public view returns (Tweet memory){
         return tweets[msg.sender][_i];
